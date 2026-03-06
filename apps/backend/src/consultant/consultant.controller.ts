80510fbe877ac99d8ba4efd49b6674e3606488be
Nolann Dubos <nolann.dubos@epitech.eu>
2026-03-06 15:55:17 +0100

    feat: swagger APIé

--- apps/backend/src/consultant/consultant.controller.ts
+++ apps/backend/src/consultant/consultant.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ConsultantService } from './consultant.service';
 
+@ApiTags('Consultants')
 @Controller('consultant')
 export class ConsultantController {
   constructor(private readonly consultantService: ConsultantService) {}
