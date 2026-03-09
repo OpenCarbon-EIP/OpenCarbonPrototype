e404957455ce7d0f5ecb6bd861fb5c1aae055658
ZifFiji <arthur.guerinault@epitech.eu>
2026-03-09 11:46:19 +0100

    feat: offer entity to have better architecture

---
+++ apps/mobile/lib/features/offers/domain/entity/offer_entity.dart
@@ -0,0 +1,60 @@
+class OfferEntity {
+  OfferEntity({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.budget,
+    required this.deadline,
+    required this.status,
+    required this.location,
+    required this.idCompany,
+    this.company,
+  });
+
+  final String id;
+  final String title;
+  final String description;
+  final double budget;
+  final String deadline;
+  final String status;
+  final String location;
+  final String idCompany;
+  final CompanyEntity? company;
+}
+
+class CompanyEntity {
+  CompanyEntity({
+    required this.id,
+    required this.companyName,
+    required this.industrySector,
+    required this.companySize,
+    required this.description,
+    required this.logoUrl,
+    required this.idUser,
+    this.user,
+  });
+
+  final String id;
+  final String companyName;
+  final String industrySector;
+  final int companySize;
+  final String description;
+  final String logoUrl;
+  final String idUser;
+  final UserEntity? user;
+}
+
+class UserEntity {
+  UserEntity({
+    required this.id,
+    required this.email,
+    required this.role,
+    required this.name,
+  });
+
+
+  final String id;
+  final String email;
+  final String role;
+  final String name;
+}
