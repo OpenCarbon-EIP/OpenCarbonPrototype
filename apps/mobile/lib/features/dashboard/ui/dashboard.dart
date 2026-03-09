2ee6ceb59c2f97853fe49bb95604da1a5104a0f6
ZifFiji <arthur.guerinault@epitech.eu>
2026-03-09 13:06:07 +0100

    fix: remove useless button

--- apps/mobile/lib/features/dashboard/ui/dashboard.dart
+++ apps/mobile/lib/features/dashboard/ui/dashboard.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_poc/ui/widgets/button.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
-  FlutterSecureStorage get _storage => const FlutterSecureStorage();
   @override
-  Widget build(BuildContext context) => Center(
-    child: SmallButton(text: 'Se déconnecter', onPressed: () async {
-      try {
-        await _storage.delete(key: 'auth_token');
-      } catch (e) {
-        print('Error deleting token: $e');
-      }
-    }),
-  );
+  Widget build(BuildContext context) => const Text('Dashboard');
 }
