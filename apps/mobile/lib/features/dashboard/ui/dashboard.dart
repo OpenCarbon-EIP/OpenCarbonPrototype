66e06f1b0c53439f8a8bd3bdadb6410c51e9b668
ZifFiji <arthur.guerinault@epitech.eu>
2026-03-09 11:38:47 +0100

    feat(temporary): button to log out

--- apps/mobile/lib/features/dashboard/ui/dashboard.dart
+++ apps/mobile/lib/features/dashboard/ui/dashboard.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_poc/core/colors/app_colors.dart';
-import 'package:flutter_poc/core/typo/app_typography.dart';
+import 'package:flutter_poc/ui/widgets/button.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
+  FlutterSecureStorage get _storage => const FlutterSecureStorage();
   @override
   Widget build(BuildContext context) => Center(
-    child: Text(
-      'Dashboard!',
-      style: AppTypography.bodyMedium.copyWith(color: AppColors.primaryLight),
-    ),
+    child: SmallButton(text: 'Se déconnecter', onPressed: () async {
+      try {
+        await _storage.delete(key: 'auth_token');
+      } catch (e) {
+        print('Error deleting token: $e');
+      }
+    }),
   );
 }
