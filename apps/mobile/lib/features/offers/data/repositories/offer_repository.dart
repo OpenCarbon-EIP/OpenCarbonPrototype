fa9f017ed05441b1111dbf2e8d6339cb2f8befa4
ZifFiji <arthur.guerinault@epitech.eu>
2026-03-09 11:45:30 +0100

    fix: use offerentity rather then the model to have a better architecture in code later

--- apps/mobile/lib/features/offers/data/repositories/offer_repository.dart
+++ apps/mobile/lib/features/offers/data/repositories/offer_repository.dart
@@ -1,21 +1,61 @@
 import 'package:flutter_poc/features/offers/data/models/offer_model.dart';
 import 'package:flutter_poc/features/offers/data/services/offer_api_service.dart';
+import 'package:flutter_poc/features/offers/data/services/offer_auth_service.dart';
+import 'package:flutter_poc/features/offers/domain/entity/offer_entity.dart';
 
 abstract class OfferRepository {
-  Future<List<OfferModel>> getOffers();
+  Future<List<OfferEntity>> getOffers();
 }
 
 class OfferRepositoryImpl implements OfferRepository {
-  OfferRepositoryImpl(this._api);
+  OfferRepositoryImpl(this._api, this._authService);
 
   final OfferApiService _api;
+  final OfferAuthService _authService;
 
   @override
-  Future<List<OfferModel>> getOffers() {
+  Future<List<OfferEntity>> getOffers() async {
     try {
-      return _api.getOffers();
-    } on Exception catch (_) {
-      throw Exception('Failed to load offers');
+      final token = await _authService.getToken();
+      final OfferModel response = await _api.getOffers(token!);
+
+      final List<OfferData> dataList = response.data ?? [];
+
+      return dataList
+          .map(
+            (offer) => OfferEntity(
+              id: offer.id,
+              title: offer.title,
+              description: offer.description,
+              budget: offer.budget,
+              deadline: offer.deadline,
+              status: offer.status,
+              location: offer.location,
+              idCompany: offer.idCompany,
+              company: offer.company != null
+                  ? CompanyEntity(
+                      id: offer.company!.id,
+                      companyName: offer.company!.companyName,
+                      industrySector: offer.company!.industrySector,
+                      companySize: offer.company!.companySize,
+                      description: offer.company!.description,
+                      logoUrl: offer.company!.logoUrl,
+                      idUser: offer.company!.idUser,
+                      user: offer.company!.user != null
+                          ? UserEntity(
+                              id: offer.company!.user!.id,
+                              email: offer.company!.user!.email,
+                              role: offer.company!.user!.role,
+                              name: offer.company!.user!.name,
+                            )
+                          : null,
+                    )
+                  : null,
+            ),
+          )
+          .toList();
+    } on Exception catch (e) {
+      throw Exception('Failed to load offers: ${e.toString()}');
     }
   }
 }
