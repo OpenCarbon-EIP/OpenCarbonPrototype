67c186bf0c9f6ba9438252c1f8ba80acf89d07ad
ZifFiji <arthur.guerinault@epitech.eu>
2026-03-10 14:58:42 +0100

    feat: dotenv package

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_poc/core/auth/auth_provider.dart';
 import 'package:flutter_poc/features/login/ui/login_view.dart';
 import 'package:flutter_poc/features/main_page/ui/main_screen_view.dart';
@@ -7,10 +8,11 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:provider/provider.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
-void main() {
+void main() async {
   const iosOptions = IOSOptions(accessibility: KeychainAccessibility.first_unlock);
   const storage = FlutterSecureStorage(iOptions: iosOptions);
 
+  await dotenv.load();
   runApp(ChangeNotifierProvider(create: (_) => AuthProvider(storage), child: const MainApp()));
 }
 
