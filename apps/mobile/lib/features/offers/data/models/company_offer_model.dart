c6083aded0edbfc169a88381386fe044b96a8781
ZifFiji <arthur.guerinault@epitech.eu>
2026-03-12 13:46:49 +0100

    fix: update type of company offer not required

--- apps/mobile/lib/features/offers/data/models/company_offer_model.dart
+++ apps/mobile/lib/features/offers/data/models/company_offer_model.dart
@@ -4,10 +4,10 @@ class CompanyOfferModel {
   CompanyOfferModel({this.success, this.data, required this.message, this.statusCode});
 
   factory CompanyOfferModel.fromJson(Map<String, dynamic> json) => CompanyOfferModel(
-    success: json['success'] as bool,
+    success: json['success'] as bool?,
     data: json['data'] != null ? OfferData.fromJson(json['data'] as Map<String, dynamic>) : null,
     message: json['message'] as String,
-    statusCode: json['statusCode'] as int,
+    statusCode: json['statusCode'] as int?,
   );
   final bool? success;
   final OfferData? data;
